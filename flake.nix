fbae8847cb870cc6d555160822b4b87c996f47b7
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-03-29 04:22:19 +0530

    fix(flake): exclude default.nix from import-tree to prevent recursion

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   outputs = inputs: let
     inherit (inputs.flake-parts.lib) mkFlake;
-    specialArgs.customLib.scanPaths = path: [ (inputs.import-tree path) ];
+    specialArgs.customLib.scanPaths = path: [ (inputs.import-tree.matchNot ".*/default\\.nix" path) ];
   in
     mkFlake {inherit inputs specialArgs;} ({inputs, ...}: {
       systems = import inputs.systems;
