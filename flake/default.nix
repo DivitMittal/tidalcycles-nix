dfe1b7dcd1cfefe6917879ece5d1a1d9855a9cdb
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-03-28 18:07:30 +0530

    chore(flake): replace local scanPaths with import-tree

--- flake/default.nix
+++ flake/default.nix
@@ -1,10 +1,3 @@
-{
-  lib,
-  ...
-}: let
-  # Custom library for scanning directories
-  customLib = import ../lib/custom.nix {inherit lib;};
-in {
-  # Auto-import all flake modules using scanPaths
+{customLib, ...}: {
   imports = customLib.scanPaths ./.;
 }
