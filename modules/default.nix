aa734d891b3bffab2eab3f3921e157c8a9b345f5
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-04-10 18:01:22 +0530

    refactor(modules): remove stub supercollider and superdirt module exports
    These modules don't exist yet; removing the references prevents broken
    imports for consumers of homeManagerModules.

--- modules/default.nix
+++ modules/default.nix
@@ -4,8 +4,6 @@ _: {
     homeManagerModules = {
       default = ./home-manager/tidalcycles.nix;
       tidalcycles = ./home-manager/tidalcycles.nix;
-      supercollider = ./home-manager/supercollider.nix;
-      superdirt = ./home-manager/superdirt.nix;
     };
 
     # NixOS modules (optional system-level config)
