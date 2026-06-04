7a05ff5fa669e232e6a05188c07fcb724c583997
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-06-04 12:49:09 +0530

    ci(flake-lock-update): remove schedule trigger, keep only workflow_dispatch

--- flake/actions/flake-lock-update.nix
+++ flake/actions/flake-lock-update.nix
@@ -4,14 +4,7 @@
   ...
 }: {
   flake.actions-nix.workflows.".github/workflows/flake-lock-update.yml" = {
-    on = {
-      workflow_dispatch = {};
-      schedule = [
-        {
-          cron = "0 0 * * 0"; # Every Sunday at midnight
-        }
-      ];
-    };
+    on.workflow_dispatch = {};
     jobs.locking-flake = {
       permissions =
         common-permissions
